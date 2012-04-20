e144e16ca4df7c62553716defab9323182d6b606
Todd Gerspacher <tgerspacher@dimpledough.com>
2012-04-20 10:52:51 -0400

    added more failing rspec tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -11,5 +11,23 @@ describe Book do
 			@book.should be_an_instance_of Book
 		end
 	end
+
+	describe "#title" do
+		it "returns the correct title" do
+			@book.title.should eql "Title"
+		end
+	end
+
+	describe "#author" do
+		it "returns the correct author" do
+			@book.author.should eql "Author"
+		end
+	end
+
+	describe "#category" do
+		it "returns the correct category" do
+			@book.category.should eql :category
+		end
+	end
 end
 
